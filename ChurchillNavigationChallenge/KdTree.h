f2d65563ca065d863b6efa13e102274c8d04a342
Eric Campbell <ericcampbell59@gmail.com>
2015-02-12 21:48:39 -0600

    cleanup

--- ChurchillNavigationChallenge/KdTree.h
+++ ChurchillNavigationChallenge/KdTree.h
@@ -51,14 +51,12 @@ static bool kd_compare_pts_y(const Point* p1, const Point* p2) {
 }
 
 static void kdtree_insert(KdTree* tree, std::vector<Point*>& pts) {
-    
     if (pts.size() == 1) {
         tree->points.push_back(pts[0]);
         return;
     }
     
     if (tree->depth >= KD_MAX_DEPTH) {
-        
         for (int i = 0; i < pts.size(); i++) {
             tree->points.push_back(pts[i]);
         }
@@ -88,19 +86,15 @@ static void kdtree_insert(KdTree* tree, std::vector<Point*>& pts) {
         Rect left_rect;
         
         if (tree->depth % 2 == 0) {
-            
             left_rect.lx = tree->bounds.lx;
             left_rect.hx = pts[median_index]->x;
             left_rect.ly = tree->bounds.ly;
             left_rect.hy = tree->bounds.hy;
-
         } else {
-            
             left_rect.lx = tree->bounds.lx;
             left_rect.hx = tree->bounds.hx;
             left_rect.ly = tree->bounds.ly;
             left_rect.hy = pts[median_index]->y;
-            
         }
         
         tree->left = kdtree_construct(left_rect, tree->depth+1);
@@ -111,19 +105,15 @@ static void kdtree_insert(KdTree* tree, std::vector<Point*>& pts) {
         Rect right_rect;
         
         if (tree->depth % 2 == 0) {
-            
             right_rect.lx = pts[median_index]->x;
             right_rect.hx = tree->bounds.hx;
             right_rect.ly = tree->bounds.ly;
             right_rect.hy = tree->bounds.hy;
-            
         } else {
-            
             right_rect.lx = tree->bounds.lx;
             right_rect.hx = tree->bounds.hx;
             right_rect.ly = pts[median_index]->y;
             right_rect.hy = tree->bounds.hy;
-            
         }
         
         tree->right = kdtree_construct(right_rect, tree->depth+1);
@@ -133,7 +123,6 @@ static void kdtree_insert(KdTree* tree, std::vector<Point*>& pts) {
 
 static inline void kdtree_return_subtree(KdTree* tree, std::priority_queue<Point*, std::vector<Point*>>& results, int& ct) {
     for (std::vector<Point*>::iterator it = tree->points.begin() ; it != tree->points.end(); ++it) {
-    
         if (results.size() < 20) {
             results.push(*it);
             ct++;
@@ -145,9 +134,7 @@ static inline void kdtree_return_subtree(KdTree* tree, std::priority_queue<Point
         } else {
             break;
         }
-        
     }
-    
 
     if (tree->left != 0) {
         kdtree_return_subtree(tree->left, results, ct);
